7a91c7f3aeb82652f6ddada06e7bfd2a077da44f
Alexander Radyushin <r.alex.andr@ya.ru>
2021-04-15 12:51:27 +0300

    fix: fixed parsing the result of 'raw' function

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-"use strict";
+'use strict'
 
-const dargs = require("dargs");
-const execa = require("execa");
+const dargs = require('dargs')
+const execa = require('execa')
 
-const { YOUTUBE_DL_PATH } = require("./constants");
+const { YOUTUBE_DL_PATH } = require('./constants')
 
-const args = (url, flags = {}) => [].concat(url, dargs(flags)).filter(Boolean);
+const args = (url, flags = {}) => [].concat(url, dargs(flags)).filter(Boolean)
 
-const isJSON = (str) => str.startsWith("{");
+const isJSON = str => str.startsWith('{')
 
-const parse = async (url, flags, opts) => {
-  const { stdout } = await execa(YOUTUBE_DL_PATH, args(url, flags), opts);
+const parse = async ({ stdout }, opts) => {
   //
   if (opts && opts.debugMode) {
-    console.log(stdout);
+    console.log(stdout)
   }
   if (isJSON(stdout)) {
     return JSON.parse(
-      opts && opts.isPlaylist ? `[${stdout.replace(/[\r\n]+/g, ",")}]` : stdout
-    );
+      opts && opts.isPlaylist ? `[${stdout.replace(/[\r\n]+/g, ',')}]` : stdout
+    )
   }
   //
-  return stdout;
-};
+  return stdout
+}
 
 const raw = (url, flags, opts) => execa(YOUTUBE_DL_PATH, args(url, flags), opts)
 
-module.exports = (url, flags, opts) => raw(url, flags, opts).then(parse)
+module.exports = (url, flags, opts) => raw(url, flags, opts).then(parse, opts)
 
 module.exports.raw = raw
 
-module.exports.args = args;
+module.exports.args = args
